4e750f7d63a0ab25991c3a9a5c2e5c760bedb413
Navya_battu <navyasree1370@gmail.com>
2023-06-20 00:10:36 +0530

    All at once

---
+++ index.js
@@ -0,0 +1,22 @@
+const fun = async()=>{
+    const response =await fetch('https://cat-fact.herokuapp.com/facts');
+    if (response.status!== 200){
+      throw new Error("couldn't able to fetch data");
+    }
+    const data =await response.json();
+    return data;
+    };
+  
+  
+    fun()
+      .then(data=>{console.log(data);
+       const maindiv =document.getElementById("datatodisplay");
+       data.forEach((item) => {
+        const addingelement= document.createElement("li");
+        addingelement.classList.add("facts");
+        addingelement.textContent=item.text;
+        maindiv.appendChild(addingelement);
+        addingelement.style.textAlign="center";
+       });
+        })
+      .catch(err=>console.log(err));
